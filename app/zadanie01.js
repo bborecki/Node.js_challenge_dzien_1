acfb7ed3286149154bbd4a6ddc76cc33fa438dd0
Bartlomiej Borecki <bborecki@gmail.com>
2018-01-30 11:44:24 +0100

    rozwiazania dzien 1

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,6 @@
 //Twój kod
+console.log('Bartłomiej Borecki');
+
+setTimeout(() => {
+  console.log('wita się z Node.js!')
+}, 5000);
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,19 @@
 //Twój kod
+const words = {
+  4000: 'Node.js',
+  1000: 'się',
+  0: 'Witam',
+  6000: 'i korzystam',
+  5000: 'w konsoli',
+  7000: 'z funkcji czasu',
+  2000: 'z',
+  3000: 'programem'
+};
+
+for (let i in words) {
+  (i => {
+    setTimeout(() => {
+      console.log(words[i])
+  }, i);
+  }) (i);
+}
